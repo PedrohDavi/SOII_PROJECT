d39a0bbaad6cdffe85d78695679dd18e9580d0a1
Pedro Davi <pedro.henrique04896@gmail.com>
2024-06-11 00:29:11 -0300

    fix

--- backend/db.js
+++ backend/db.js
@@ -14,7 +14,7 @@ async function createDatabase() {
     let conn;
     try {
         conn = await pool.getConnection();
-        await conn.query("CREATE DATABASE IF NOT EXISTS so2");
+        await conn.query("CREATE DATABASE IF NOT EXISTS so2;");
     } catch (err) {
         console.error("Erro ao criar/verificar banco de dados:", err);
     } finally {
@@ -35,8 +35,8 @@ async function createTables() {
     let conn;
     try {
         conn = await db.getConnection();
-        await conn.query("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255) NOT NULL, usuario VARCHAR(255) NOT NULL, senha VARCHAR(255) NOT NULL)");
-        await conn.query("CREATE TABLE IF NOT EXISTS reserva (id INT AUTO_INCREMENT PRIMARY KEY, nome_sala VARCHAR(250), local_sala VARCHAR(250), data_uso DATE, hora_inicio_uso TIME, hora_final_uso TIME, responsavel VARCHAR(250), motivo_uso VARCHAR(450), info_gerais VARCHAR(450), convidados VARCHAR(250), usuario_id INT NOT NULL, FOREIGN KEY (usuario_id) REFERENCES users(id))");
+        await conn.query("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255) NOT NULL, usuario VARCHAR(255) NOT NULL, senha VARCHAR(255) NOT NULL);");
+        await conn.query("CREATE TABLE IF NOT EXISTS reserva (id INT AUTO_INCREMENT PRIMARY KEY, nome_sala VARCHAR(250), local_sala VARCHAR(250), data_uso DATE, hora_inicio_uso TIME, hora_final_uso TIME, responsavel VARCHAR(250), motivo_uso VARCHAR(450), info_gerais VARCHAR(450), convidados VARCHAR(250), usuario_id INT NOT NULL, FOREIGN KEY (usuario_id) REFERENCES users(id));");
         console.log("Tabelas criadas");
     } catch (err) {
         console.error("Erro ao criar/verificar tabelas:", err);
